44ce41075e65223d3f45233e3bc701e5566d5b50
Bharathpillai06 <62036034+Bharathpillai06@users.noreply.github.com>
2025-12-10 21:15:24 -0500

    Update Playlist1L.java

---
+++ Portfolio-main/src/Playlist1L.java
@@ -0,0 +1,68 @@
+import components.queue.Queue;
+import components.queue.Queue1L;
+/**
+ * Implementation of the SpellBook component using the OSU API.
+ */
+
+public final class Playlist1L extends PlaylistSecondary {
+
+    /**
+     * Underlying representation: a queue of songs.
+     * The front of the queue is the front of the playlist.
+     */
+    private Queue<Song> rep;
+
+    /**
+     * Creates a new, empty representation.
+     */
+    private void createNewRep() {
+        this.rep = new Queue1L<>();
+    }
+
+    /**
+     * Default constructor: initializes this to an empty playlist.
+     */
+    public Playlist1L() {
+        this.createNewRep();
+    }
+
+    @Override
+    public void add(Song song) {
+        assert song != null : "Violation of: song is not null";
+        this.rep.enqueue(song); }
+
+    @Override
+    public Song remove() {
+        assert !this.isEmpty() : "Violation of: this is not empty";
+        return this.rep.dequeue(); 
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.rep.length() == 0;
+    }
+
+   
+
+    @Override
+    public void clear() {
+        this.createNewRep();
+    }
+
+    @Override
+    public Playlist newInstance() {
+        return new Playlist1L();
+    }
+
+    @Override
+    public void transferFrom(Playlist source) {
+        assert source != null : "Violation of: source is not null";
+        assert source != this : "Violation of: source is not this";
+        assert source instanceof Playlist1L
+                : "Violation of: source is of dynamic type Playlist1L";
+
+        Playlist1L local = (Playlist1L) source;
+        this.rep = local.rep;
+        local.createNewRep();
+    }
+}
